6d01f308361fb2b1da6c4e71a166b743d2313149
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-10-03 22:42:36 +0200

    Добавьте файлы проекта.

---
+++ lab2.1Popov/Program.cs
@@ -0,0 +1,6 @@
+﻿//высокий 
+Console.Write("Введите x");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine((x <= 2) && (x >= -2) && (y <= 1) && (y >= -1) && (y * y + x * x >= 1));
